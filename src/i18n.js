814b74412ec2d2d0ccffb42be1fe827075b104b4
t-tlean <striguntima@icloud.com>
2025-03-10 01:53:45 +0200

    3

--- src/i18n.js
+++ src/i18n.js
@@ -14,7 +14,7 @@ i18n
       caches: ["localStorage"],
     },
     backend: {
-      loadPath: "/locales/{{lng}}/translation.json", // Загружает файлы из public/locales
+      loadPath: "locales/{{lng}}/translation.json", // Загружает файлы из public/locales
     },
     interpolation: { escapeValue: false },
   });
